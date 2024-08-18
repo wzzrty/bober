705cba401b078d869b4787e2bd6695459d6773c0
SATOSHI <126672771+wzzrty@users.noreply.github.com>
2024-08-18 18:56:37 +0300

    update.sh

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 cd
-rm -rf Spavveri
+rm -rf bober
 git clone https://github.com/wzzrty/bober.git
 cd
-rm -rf Spavveri
+rm -rf bober
 git clone https://github.com/wzzrty/bober.git
 cd
-cd Spavveri
-python3 Spavveri.py
+cd bober
+python Bober.py
